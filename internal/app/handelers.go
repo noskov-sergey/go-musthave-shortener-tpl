b4ab4b9cc818a011161311e824476c2813158438
Noskov Sergey <noskov.sergey@mail.ru>
2024-02-04 17:11:41 +0500

    Changes for tests iter7 v2

--- internal/app/handelers.go
+++ internal/app/handelers.go
@@ -73,6 +73,6 @@ func APIShorten(res http.ResponseWriter, req *http.Request) {
 	}
 
 	res.Header().Set("Content-Type", "application/json")
-	res.WriteHeader(http.StatusOK)
+	res.WriteHeader(http.StatusCreated)
 	res.Write(resp)
 }
