b0505e3660be311ad8efa6bda63041f15f5d94b3
Noskov Sergey <noskov.sergey@mail.ru>
2024-02-06 21:25:37 +0500

    fix bug

--- internal/app/router.go
+++ internal/app/router.go
@@ -2,13 +2,14 @@ package server
 
 import (
 	"github.com/go-chi/chi/v5"
+	"go-musthave-shortener-tpl/internal/app/compress"
 	"go-musthave-shortener-tpl/internal/app/logger"
 )
 
 func LinkRouter() chi.Router {
 	r := chi.NewRouter()
 	r.Use(logger.Logger())
-	// r.Use(compress.GzipMiddleware())
+	r.Use(compress.GzipMiddleware())
 	r.Route("/", func(r chi.Router) {
 		r.Post("/", CreateRedirect)
 		r.Get("/{shortlink}", Redirect)
