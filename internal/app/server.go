cd2909db9dccc5336736a024ebdbc233bb21367d
Noskov Sergey <noskov.sergey@mail.ru>
2024-03-01 17:56:36 +0500

    log for debug2

--- internal/app/server.go
+++ internal/app/server.go
@@ -1,14 +1,10 @@
 package storage
 
 import (
-	"context"
 	"go-musthave-shortener-tpl/internal/app/config"
 	"go-musthave-shortener-tpl/internal/app/logger"
 	"go.uber.org/zap"
-	"log"
 	"net/http"
-	"os"
-	"os/signal"
 )
 
 func RunServer(params *config.NetAddress) error {
@@ -16,24 +12,10 @@ func RunServer(params *config.NetAddress) error {
 	srv.Addr = params.String()
 	srv.Handler = LinkRouter()
 
-	idleConnsClosed := make(chan struct{})
-	go func() {
-		sigint := make(chan os.Signal, 1)
-		signal.Notify(sigint, os.Interrupt)
-		<-sigint
-		if err := srv.Shutdown(context.Background()); err != nil {
-			log.Printf("HTTP server Shutdown: %v", err)
-		}
-		close(idleConnsClosed)
-	}()
-
 	if err := logger.Initialize(); err != nil {
 		return err
 	}
 	logger.Sugar.Info("Running server", zap.String("address", params.String()))
 
 	return srv.ListenAndServe()
-
-	<-idleConnsClosed
-	return nil
 }
